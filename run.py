58f83d6cff436d3009f94db90a02d9d86b81244a
mrobatis <moh.robati1333@gmail.com>
2022-12-08 06:00:58 -0500

    multicored

--- run.py
+++ run.py
@@ -1,63 +1,77 @@
 from path import *
 from to_json import *
 import os, json
+import threading
 
-dataset = open("./benchmark/train.jsonl", "r")
-final_results_file = open("./__logs__/results.txt", "w+")
+dataset = open("./benchmark/test.jsonl", "r").readlines()
 hash_table = json.loads(open("./embedding/hash_table.json", "r").read())
-curr_file = None
-curr_indices = []
-curr_types = []
 
-def mine_binary_info():
-    print("Starting to mine:", curr_file)
-    success = parse_binary_to_json(curr_file.replace("../../binaries/wasm-dwarf/", "../data/wasm-dwarf/"))
-    if not success:
-        print("Mining timed out!\n===")
-        return False
-    json_file = open("./__logs__/parsed_json.json", "r")
-    wat_dict = json.loads(json_file.read())
-    json_file.close()
-    paths = get_paths(wat_dict)
-    import_count, import_done_idx = count_import_functions(paths)
-    functions = get_function_paths(paths[import_done_idx:], curr_indices, import_count)
-    for func in functions:
-        ty = None
-        if len(curr_types) > 0:
-            ty = curr_types.pop(0)
-        vector = [0 for i in range(len(hash_table))]
-        for path in func:
-            if path[5:] in hash_table:
-                vector[hash_table[path[5:]]] += 1
-        final_results_file.write(str([vector, ty]) + "\n")
-    print("Mining done!\n===")
-    return True
+THREAD_SIZE = 32
 
-try:
-    for line in dataset:
-        [info, ty] = line.split("|DELIMETER973d1544f1|")
-        ty = ty.replace("\n", "")
-        datapoint = json.loads(info)
-        if curr_file is None:
-            curr_file = datapoint['file']
-        if curr_file == datapoint['file']:
-            curr_indices.append(datapoint['function_idx'])
-            curr_types.append(ty)
-        else:
-            mine_binary_info()
-            curr_file = datapoint['file']
-            curr_indices = []
-            curr_types = []
-            curr_indices.append(datapoint['function_idx'])
-            curr_types.append(ty)
+class Collector(threading.Thread):
 
-    if len(curr_indices):
-        mine_binary_info()
-        
-        
-    dataset.close()
-    final_results_file.close()
-except:
-        
-    dataset.close()
-    final_results_file.close()
+    def __init__(self, t_id):
+        super(Collector, self).__init__()
+        window_size = int(len(dataset) / THREAD_SIZE)
+        self.t_id=t_id
+        self.low=t_id*window_size
+        self.high=(t_id+1)*window_size
+        self.final_results = []
+        self.curr_file = None
+        self.curr_indices = []
+        self.curr_types = []
+    
+    def mine_binary_info(self):
+        print("Starting to mine:", self.curr_file)
+        success = parse_binary_to_json(self.curr_file.replace("../../binaries/wasm-dwarf/", "../data/wasm-dwarf/"), self.t_id)
+        if not success:
+            print("Mining timed out!\n===")
+            return False
+        json_file = open("./__logs__/parsed_json_{}.json".format(self.t_id), "r")
+        wat_dict = json.loads(json_file.read())
+        json_file.close()
+        paths = get_paths(wat_dict)
+        import_count, import_done_idx = count_import_functions(paths)
+        functions = get_function_paths(paths[import_done_idx:], self.curr_indices, import_count)
+        for func in functions:
+            ty = None
+            if len(self.curr_types) > 0:
+                ty = self.curr_types.pop(0)
+            vector = [0 for i in range(len(hash_table))]
+            for path in func:
+                if path[5:] in hash_table:
+                    vector[hash_table[path[5:]]] += 1
+            self.final_results.append(str([vector, ty]) + "\n")
+        print("Mining done!\n===")
+        return True
+
+    def run(self):
+        for line in dataset[self.low:self.high]:
+            [info, ty] = line.split("|DELIMETER973d1544f1|")
+            ty = ty.replace("\n", "")
+            datapoint = json.loads(info)
+            if self.curr_file is None:
+                self.curr_file = datapoint['file']
+            if self.curr_file == datapoint['file']:
+                self.curr_indices.append(datapoint['function_idx'])
+                self.curr_types.append(ty)
+            else:
+                self.mine_binary_info()
+                self.curr_file = datapoint['file']
+                self.curr_indices = []
+                self.curr_types = []
+                self.curr_indices.append(datapoint['function_idx'])
+                self.curr_types.append(ty)
+        if len(self.curr_indices):
+            self.mine_binary_info()
+
+collectors = [Collector(i) for i in range(THREAD_SIZE)]
+for c in collectors:
+    c.start()
+for c in collectors:
+    c.join()
+f_results = open("./__logs__/results.csv", "w+")
+for c in collectors:
+    for line in c.final_results:
+        f_results.write(line.replace("[[","").replace(" ","").replace("],'",",").replace("']",""))
+
--- to_json.py
+++ to_json.py
@@ -2,28 +2,28 @@ from syn_compiler.lexer import Lexer
 from syn_compiler.parser import Parser
 import os, glob, sys, multiprocessing
 
-def do_work(filename, return_dict):
+def do_work(filename, return_dict, t_id):
     return_dict['success'] = None
-    _ = os.system("cargo run --quiet " + filename)
-    f_debug = open("./__logs__/parsed_debug.txt", "r")
+    _ = os.system("cargo run --quiet " + filename + " " + str(t_id))
+    f_debug = open("./__logs__/parsed_debug_{}.txt".format(t_id), "r")
     input = f_debug.read()
     f_debug.close()
     lexer = Lexer().build()
     lexer.input(input)
     parser = Parser()
     parser.build().parse(input, lexer, False)
-    f_json = open("./__logs__/parsed_json.json", "w+")
+    f_json = open("./__logs__/parsed_json_{}.json".format(t_id), "w+")
     f_json.write(parser.dictTree_string)
     f_json.close()
     return_dict['success'] = True
 
 # target_file = './projects/abcm2ps/**/music.o'
 
-def parse_binary_to_json(target_file):
+def parse_binary_to_json(target_file, t_id):
     manager = multiprocessing.Manager()
     return_dict = manager.dict()
     for filename in [glob.glob(target_file, recursive=True)[0]]:
-        proc = multiprocessing.Process(target=do_work, args=(filename, return_dict))
+        proc = multiprocessing.Process(target=do_work, args=(filename, return_dict, t_id))
         proc.start()
         proc.join(timeout=600)
         proc.terminate()
